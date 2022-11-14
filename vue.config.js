f0ed6f3351601bcd6c8d91fdfed0c602a7ced295
zhanghengxin <ezreal.ice@icloud.com>
2022-11-15 01:04:25 +0800

    the project init

---
+++ vue.config.js
@@ -0,0 +1,23 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  // 修改src目录为examples目录
+  pages: {
+    index: {
+      entry: 'examples/main.js',
+      template: 'public/index.html',
+      filename: 'index.html'
+    }
+  },
+  // 扩展webpack配置，使packages加入编译
+  chainWebpack: config => {
+    config.module
+        .rule('js')
+        .include
+        .add('/packages')
+        .end()
+        .use('babel')
+        .loader('babel-loader')
+        .tap(options => options);
+  }
+})
